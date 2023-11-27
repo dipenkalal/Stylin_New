66cbf68b32b4b9be286664f890c033283d9fb0c7
Dipen Kalal <57854287+dipenkalal@users.noreply.github.com>
2023-11-27 01:22:41 -0500

    .

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import atexit
+import datetime
 from decouple import config
 import pymongo
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -13,6 +15,11 @@ import os
 from werkzeug.utils import secure_filename
 import json
 from pymongo.errors import BulkWriteError
+from marshmallow import Schema, fields, validate, ValidationError
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+import requests
+from apscheduler.schedulers.background import BackgroundScheduler
 
 # MongoDB connection string goes here.
 uri = config('MONGO_URI')
@@ -20,6 +27,10 @@ myclient = pymongo.MongoClient(uri)
 mydb = myclient['Stylin8']
 mycol = mydb['User']
 clothes_collection = mydb['Clothes'] 
+
+#OpenWeather API
+OPENWEATHER_API_KEY = config('OPENWEATHER_API_KEY')
+
 
 #initializing the app
 app = Flask(__name__)
@@ -42,6 +53,27 @@ app.config["MAIL_USE_SSL"] = True
 
 mail = Mail(app)
 
+#registration schema 
+class RegisterSchema(Schema):
+    fname = fields.Str(required=True, validate=validate.Length(min=1))
+    lname = fields.Str(required=True, validate=validate.Length(min=1))
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, validate=validate.Length(min=6))
+
+#Login Schema
+class LoginSchema(Schema):
+    email = fields.Email(required=True)
+    password = fields.Str(required=True, validate=validate.Length(min=6))
+
+# # Initialize the limiter
+# limiter = Limiter(
+#     app,
+#     key_func=get_remote_address,
+#     default_limits=["200 per day", "50 per hour"]
+# )
+
+
+
 #image_upload
 def allowed_file(filename):
     return '.' in filename and \
@@ -62,9 +94,12 @@ def upload_clothe():
         return jsonify({'error': 'No category provided'}), 400
     if file.filename == '':
         return jsonify({'error': 'No file selected for uploading'}), 400
+    if not allowed_file(file.filename):
+        return jsonify({'error': 'Allowed file types are .jpg, .png, .jpeg'}), 400
+    
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_PATH'], filename)
+        file_path = os.path.join(app.config['   UPLOAD_PATH'], filename)
         file.save(file_path)
 
         # Insert file path, category, and tags into the MongoDB database
@@ -96,12 +131,12 @@ def add_clothe_to_db(user_id, file_path, category):
 
 #Register_Page API
 @app.route("/Register", methods=["POST"])
+# @limiter.limit("5 per minute")
 def register():
-    content = request.get_json()
-
-#Basic validation.
-    if not all(key in content for key in ["fname", "lname", "email", "password"]):
-        return jsonify({"error": "Missing required fields"}), 400
+    try:
+        content = RegisterSchema().load(request.get_json())
+    except ValidationError as err:
+        return jsonify(err.messages), 400
 
 # Check if user already exists
     existing_user = mycol.find_one({"email": content["email"]})
@@ -118,23 +153,36 @@ def register():
 
 #Login_Page API
 @app.route("/Login", methods=["POST"])
+# @limiter.limit("10 per minute")
 def login():
-    content = request.get_json()
-    if not all(key in content for key in ["email", "password"]):
-        return jsonify({"error": "Missing email or password"}), 400
+    try:
+        # Validate the input data using the LoginSchema
+        content = LoginSchema().load(request.get_json())
+    except ValidationError as err:
+        # Return validation errors
+        return jsonify(err.messages), 400
 
-    user = mycol.find_one({"email": content["email"]})
+    try:
+        user = mycol.find_one({"email": content["email"]})
 
-    if not user:
-        return jsonify({"error": "Email not found"}), 401
+        if not user:
+            return jsonify({"error": "Email not found"}), 401
 
-    if not check_password_hash(user["password"], content["password"]):
-        return jsonify({"error": "Wrong password"}), 401
+        if not check_password_hash(user["password"], content["password"]):
+            return jsonify({"error": "Wrong password"}), 401
 
-#If email and password are Verified, returning successful message
-    session['email'] = content["email"]
-    return jsonify({"message": "Login successful!"}), 200
-    
+        # If email and password are verified, you can add additional logic here
+        # ...
+
+        return jsonify({"message": "Login successful!"}), 200
+
+    except pymongo.errors.OperationFailure as e:
+        return jsonify({"error": "Database operation failed"}), 500
+    except Exception as e:
+        return jsonify({"error": "An error occurred"}), 500
+
+
+
 #Logout API    
 @app.route('/Logout', methods=['POST'])
 def logout():
@@ -331,6 +379,109 @@ def import_user_data():
         except BulkWriteError as bwe:
             # Return detailed error message if a BulkWriteError occurs
             return jsonify({'error': str(bwe.details)}), 500
+
+
+
+@app.route('/get_weather', methods=['GET'])
+def get_weather():
+    # Get the city name from the request
+    city = request.args.get('city', 'New York')  # Default to New York if no city is provided
+
+    # OpenWeatherMap API endpoint with city
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHER_API_KEY}&units=metric'
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Will raise an HTTPError if the HTTP request returned an unsuccessful status code
+
+        data = response.json()
+
+        # Create a document to save in the database
+        weather_document = {
+            'city': city,
+            'temperature': data['main']['temp'],
+            'weather_conditions': data['weather'][0]['description'],
+            'last_updated': datetime.datetime.utcnow()  # Store the current time
+        }
+
+        # Save or update the document in the database
+        mydb['weather_data'].update_one({'city': city}, {'$set': weather_document}, upsert=True)
+
+        return jsonify({'temperature': temperature, 'weather_conditions': weather_conditions}), 200
+    
+    except requests.exceptions.HTTPError as errh:
+        return jsonify({'error': f'HTTP Error: {errh}'}), errh.response.status_code
+    except requests.exceptions.ConnectionError as errc:
+        return jsonify({'error': f'Error Connecting: {errc}'}), 500
+    except requests.exceptions.Timeout as errt:
+        return jsonify({'error': f'Timeout Error: {errt}'}), 500
+    except requests.exceptions.RequestException as err:
+        return jsonify({'error': f'Error: {err}'}), 500
+    
+
+# Define the function to update weather data
+def update_weather_data():
+    # List of cities to update
+    cities = ["Windsor", "London", "Cambridge"]  # Add your list of cities here
+
+    for city in cities:
+        weather_data = get_weather,
+        if weather_data:
+            mydb['weather_data'].update_one({'city': city}, {'$set': weather_data}, upsert=True)
+
+# Initialize the scheduler
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=update_weather_data, trigger="interval", hours=1)
+scheduler.start()
+
+# Ensure that the scheduler is shut down when exiting the app
+atexit.register(lambda: scheduler.shutdown())
+
+
+@app.route('/add_favorite', methods=['POST'])
+def add_favorite():
+    user_id = request.json.get('user_id')
+    clothe_id = request.json.get('clothe_id')
+    # Add more fields if necessary
+
+    # Insert into favorites collection
+    mydb['favorites'].insert_one({'user_id': user_id, 'clothe_id': clothe_id})
+    return jsonify({'message': 'Added to favorites'}), 200
+
+@app.route('/get_favorites', methods=['GET'])
+def get_favorites():
+    user_id = request.args.get('user_id')
+
+    favorites = mydb['favorites'].find({'user_id': user_id})
+    favorite_items = [item for item in favorites]
+
+    return jsonify(favorite_items), 200
+
+
+
+def recommend_clothes(user_id):
+    # Example logic: find the most favorited category of clothes for this user
+    favorite_category = mydb['favorites'].aggregate([
+        {'$match': {'user_id': user_id}},
+        {'$group': {'_id': '$category', 'count': {'$sum': 1}}},
+        {'$sort': {'count': -1}},
+        {'$limit': 1}
+    ])
+
+    if favorite_category:
+        category = favorite_category[0]['_id']
+        recommended_items = mydb['clothes'].find({'category': category}).limit(10)
+        return [item for item in recommended_items]
+    else:
+        return []
+
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # logging the exception here for debugging purposes
+    print(f"An error occurred: {str(e)}")
+    return jsonify({'error': 'A server error occurred'}), 500
 
 
 if __name__ == "__main__":
